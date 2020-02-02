a6440a5282260251262b020945f5b6ea461235f5
neo0801 <neo081@outlook.com>
2020-02-02 15:45:59 +0800

    增加multi language例子

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -2,13 +2,41 @@ package main
 
 import "fmt"
 
-const prefixWord = "Hello, "
+const prefixWordInEnglish = "Hello, "
+const prefixWordInChinese = "你好, "
+const prefixWordInSpanish = "Hola, "
+
+func GetDefaultName(language string) string {
+	name := "World"
+	switch language {
+	case "chinese":
+		name = "世界"
+	case "spanish":
+		name = "Elodie"
+	}
+	return name
+}
+
+// GetGreetingPrefix returns hello prefix according to language string
+func GetGreetingPrefix(language string) string {
+	prefix := prefixWordInEnglish
+	switch language {
+	case "chinese":
+		prefix = prefixWordInChinese
+	case "spanish":
+		prefix = prefixWordInSpanish
+	}
+	return prefix
+}
 
 // Hello just return hello world!
-func Hello(name string) string {
-	return prefixWord + name
+func Hello(name, languange string) string {
+	if name == "" {
+		name = GetDefaultName(languange)
+	}
+	return GetGreetingPrefix(languange) + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("", ""))
 }
--- helloworld/helloworld_test.go
+++ helloworld/helloworld_test.go
@@ -5,18 +5,35 @@ import (
 	"testing"
 )
 
-// TestHello tests Hello function
-func TestHello(t *testing.T) {
-	got := Hello("world!")
-	want := "Hello, world!"
-
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got '%q' want '%q'", got, want)
 	}
 }
 
+// TestHello tests Hello function
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		var tmp string
+		got := Hello(tmp, "chinese")
+		want := "你好, 世界"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
 func BenchmarkHello(b *testing.B) {
-	for i := 0; i<b.N; i++ {
-		fmt.Println(Hello("benchmark!"))
+	for i := 0; i < b.N; i++ {
+		fmt.Println(Hello("benchmark!", ""))
 	}
 }
